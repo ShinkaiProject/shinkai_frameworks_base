e64a3b84209453eaaddadb08807964f41818e2ae
Romain Guy <romainguy@android.com>
2009-12-04 12:07:53 -0800

    am a1a68a4a: am f8875f48: Merge change If32b488b into eclair-mr2
    Merge commit 'a1a68a4a2d1b1c48b3dc98638cb2e498b6f65408'
    
    * commit 'a1a68a4a2d1b1c48b3dc98638cb2e498b6f65408':
      Add new unit test for RadioGroup/RadioButton.

---
+++ include/media/stagefright/HTTPDataSource.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef HTTP_DATASOURCE_H_
+
+#define HTTP_DATASOURCE_H_
+
+#include <media/stagefright/DataSource.h>
+
+namespace android {
+
+class HTTPStream;
+
+class HTTPDataSource : public DataSource {
+public:
+    HTTPDataSource(const char *host, int port, const char *path);
+    HTTPDataSource(const char *uri);
+
+    virtual status_t initCheck() const;
+
+    virtual ssize_t readAt(off_t offset, void *data, size_t size);
+
+protected:
+    virtual ~HTTPDataSource();
+
+private:
+    enum {
+        kBufferSize = 64 * 1024
+    };
+
+    HTTPStream *mHttp;
+    char *mHost;
+    int mPort;
+    char *mPath;
+
+    void *mBuffer;
+    size_t mBufferLength;
+    off_t mBufferOffset;
+
+    status_t mInitCheck;
+
+    HTTPDataSource(const HTTPDataSource &);
+    HTTPDataSource &operator=(const HTTPDataSource &);
+};
+
+}  // namespace android
+
+#endif  // HTTP_DATASOURCE_H_
+
