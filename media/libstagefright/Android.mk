e9b73ccc73ef80561bc202902fd950f2377a5bd0
Grace Kloba <klobag@google.com>
2009-12-07 13:40:53 -0800

    am ddcea3d2: Expose onReceivedSslError() so that the WebView client can accept the error and continue.
    Merge commit 'ddcea3d21645bb3037703c84807dae077aae62ce' into eclair-mr2-plus-aosp
    
    * commit 'ddcea3d21645bb3037703c84807dae077aae62ce':
      Expose onReceivedSslError() so that the WebView client

---
+++ media/libstagefright/Android.mk
@@ -0,0 +1,73 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:=                 \
+        ESDS.cpp                  \
+        MediaBuffer.cpp           \
+        MediaBufferGroup.cpp      \
+        MediaDefs.cpp             \
+        MediaSource.cpp           \
+        MetaData.cpp              \
+        OMXCodec.cpp              \
+        Utils.cpp                 \
+        OMXClient.cpp
+
+ifeq ($(BUILD_WITH_FULL_STAGEFRIGHT),true)
+
+LOCAL_SRC_FILES +=                \
+        AMRExtractor.cpp          \
+        AudioPlayer.cpp           \
+        AwesomePlayer.cpp         \
+        CachingDataSource.cpp     \
+        CameraSource.cpp          \
+        DataSource.cpp            \
+        FileSource.cpp            \
+        HTTPDataSource.cpp        \
+        HTTPStream.cpp            \
+        JPEGSource.cpp            \
+        MP3Extractor.cpp          \
+        MPEG4Extractor.cpp        \
+        MPEG4Writer.cpp           \
+        MediaExtractor.cpp        \
+        SampleTable.cpp           \
+        ShoutcastSource.cpp       \
+        TimeSource.cpp            \
+        TimedEventQueue.cpp       \
+        WAVExtractor.cpp          \
+        string.cpp
+
+LOCAL_CFLAGS += -DBUILD_WITH_FULL_STAGEFRIGHT
+endif
+
+LOCAL_C_INCLUDES:= \
+	$(JNI_H_INCLUDE) \
+        $(TOP)/external/opencore/extern_libs_v2/khronos/openmax/include \
+        $(TOP)/external/opencore/android
+
+LOCAL_SHARED_LIBRARIES := \
+        libbinder         \
+        libmedia          \
+        libutils          \
+        libcutils         \
+        libui
+
+ifeq ($(BUILD_WITH_FULL_STAGEFRIGHT),true)
+
+LOCAL_STATIC_LIBRARIES := \
+        libstagefright_aacdec
+
+endif
+
+ifeq ($(TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
+        LOCAL_LDLIBS += -lpthread
+endif
+
+LOCAL_CFLAGS += -Wno-multichar
+
+LOCAL_PRELINK_MODULE:= false
+
+LOCAL_MODULE:= libstagefright
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
