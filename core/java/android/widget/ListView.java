ShinkaiProject/shinkai_frameworks_base

commit 7f3850945ceea6cc290e7958a2dec9d4e362effc

Revert "Fix ListView is not scrolled properly with arrows"
This reverts commit 006a90384ed5e7210f6daf2384af6d7171f92676.

core/java/android/widget/ListView.java
@@ -2429,9 +2429,7 @@ public class ListView extends AbsListView {
         View selectedView = getSelectedView();
         int selectedPos = mSelectedPosition;
 
-        int nextSelectedPosition = (direction == View.FOCUS_DOWN) ?
-                lookForSelectablePosition(selectedPos + 1, true) :
-                lookForSelectablePosition(selectedPos - 1, false);
+        int nextSelectedPosition = lookForSelectablePositionOnScreen(direction);
         int amountToScroll = amountToScroll(direction, nextSelectedPosition);
 
         // if we are moving focus, we may OVERRIDE the default behavior
@@ -2643,18 +2641,14 @@ public class ListView extends AbsListView {
         final int listBottom = getHeight() - mListPadding.bottom;
         final int listTop = mListPadding.top;
 
-        int numChildren = getChildCount();
+        final int numChildren = getChildCount();
 
         if (direction == View.FOCUS_DOWN) {
             int indexToMakeVisible = numChildren - 1;
             if (nextSelectedPosition != INVALID_POSITION) {
                 indexToMakeVisible = nextSelectedPosition - mFirstPosition;
             }
-            while (numChildren <= indexToMakeVisible) {
-                // Child to view is not attached yet.
-                addViewBelow(getChildAt(numChildren - 1), mFirstPosition + numChildren - 1);
-                numChildren++;
-            }
+
             final int positionToMakeVisible = mFirstPosition + indexToMakeVisible;
             final View viewToMakeVisible = getChildAt(indexToMakeVisible);
 
@@ -2686,12 +2680,6 @@ public class ListView extends AbsListView {
         } else {
             int indexToMakeVisible = 0;
             if (nextSelectedPosition != INVALID_POSITION) {
-                indexToMakeVisible = nextSelectedPosition - mFirstPosition;
-            }
-            while (indexToMakeVisible < 0) {
-                // Child to view is not attached yet.
-                addViewAbove(getChildAt(0), mFirstPosition);
-                mFirstPosition--;
                 indexToMakeVisible = nextSelectedPosition - mFirstPosition;
             }
             final int positionToMakeVisible = mFirstPosition + indexToMakeVisible;

commit 08d9d5b3ccc4e79228f9d541edf9c04d5458b398

Fix a stability regression in ListView arrow scrolling
The earlier patch 7f3850945 introduced a stability regression. Perform
the appropriate bounds checking alongside the fix it implements.

Bug 8264185

Change-Id: I943d6c05bacdd777f89243fdac97788b16639dc6

core/java/android/widget/ListView.java
@@ -2417,6 +2417,34 @@ public class ListView extends AbsListView {
     }
 
     /**
+     * Used by {@link #arrowScrollImpl(int)} to help determine the next selected position
+     * to move to. This can return a position currently not represented by a view on screen
+     * but only in the direction given.
+     *
+     * @param selectedPos Current selected position to move from
+     * @param direction Direction to move in
+     * @return Desired selected position after moving in the given direction
+     */
+    private final int nextSelectedPositionForDirection(int selectedPos, int direction) {
+        int nextSelected;
+        if (direction == View.FOCUS_DOWN) {
+            nextSelected = selectedPos != INVALID_POSITION && selectedPos >= mFirstPosition ?
+                    selectedPos + 1 :
+                    mFirstPosition;
+        } else {
+            final int lastPos = mFirstPosition + getChildCount() - 1;
+            nextSelected = selectedPos != INVALID_POSITION && selectedPos < lastPos?
+                    selectedPos - 1 :
+                    lastPos;
+        }
+
+        if (nextSelected < 0 || nextSelected >= mAdapter.getCount()) {
+            return INVALID_POSITION;
+        }
+        return lookForSelectablePosition(nextSelected, direction == View.FOCUS_DOWN);
+    }
+
+    /**
      * Handle an arrow scroll going up or down.  Take into account whether items are selectable,
      * whether there are focusable items etc.
      *
@@ -2431,9 +2459,7 @@ public class ListView extends AbsListView {
         View selectedView = getSelectedView();
         int selectedPos = mSelectedPosition;
 
-        int nextSelectedPosition = (direction == View.FOCUS_DOWN) ?
-                lookForSelectablePosition(selectedPos + 1, true) :
-                lookForSelectablePosition(selectedPos - 1, false);
+        int nextSelectedPosition = nextSelectedPositionForDirection(selectedPos, direction);
         int amountToScroll = amountToScroll(direction, nextSelectedPosition);
 
         // if we are moving focus, we may OVERRIDE the default behavior