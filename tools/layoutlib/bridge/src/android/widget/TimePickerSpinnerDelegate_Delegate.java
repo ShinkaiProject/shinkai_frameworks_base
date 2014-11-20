ShinkaiProject/shinkai_frameworks_base

commit 954e273039f853d55159ef8e4ffa611220346f54

LayoutLib: Stub AM/PM keycodes in Time Picker.
TimePickerSpinnerDelegate doesn't need to deal with input when running
inside LayoutLib. So, we just return the keycodes as if it's running in
English locale.

Bug: http://b.android.com/79189
Change-Id: Ife0ae91a1e34a8dac379bc1dda1c1c8ed461c164

tools/layoutlib/bridge/src/android/widget/TimePickerSpinnerDelegate_Delegate.java
@@ -1,0 +1,44 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.widget;
+
+import com.android.tools.layoutlib.annotations.LayoutlibDelegate;
+
+import android.view.KeyEvent;
+
+/**
+ * Delegate used to provide new implementation of few methods in {@link TimePickerSpinnerDelegate}.
+ */
+public class TimePickerSpinnerDelegate_Delegate {
+
+    // Copied from TimePickerSpinnerDelegate.
+    private static final int AM = 0;
+    private static final int PM = 1;
+
+    @LayoutlibDelegate
+    static int getAmOrPmKeyCode(int amOrPm) {
+        // We don't care about locales here.
+        if (amOrPm == AM) {
+            return KeyEvent.KEYCODE_A;
+        } else if (amOrPm == PM) {
+            return KeyEvent.KEYCODE_P;
+        } else {
+            assert false : "amOrPm value in TimePickerSpinnerDelegate can only be 0 or 1";
+            return -1;
+        }
+    }
+}

core/java/android/widget/TimePickerSpinnerDelegate.java
@@ -61,6 +61,8 @@ class TimePickerSpinnerDelegate extends TimePicker.AbstractTimePickerDelegate im
     // Also NOT a real index, just used for keyboard mode.
     private static final int ENABLE_PICKER_INDEX = 3;
 
+    // LayoutLib relies on these constants. Change TimePickerSpinnerDelegate_Delegate if
+    // modifying these.
     private static final int AM = 0;
     private static final int PM = 1;
 

tools/layoutlib/create/src/com/android/tools/layoutlib/create/CreateInfo.java
@@ -167,6 +167,7 @@ public final class CreateInfo implements ICreateInfo {
         "android.view.RenderNode#nSetElevation",
         "android.view.RenderNode#nGetElevation",
         "android.view.ViewGroup#drawChild",
+        "android.widget.TimePickerSpinnerDelegate#getAmOrPmKeyCode",
         "com.android.internal.view.menu.MenuBuilder#createNewMenuItem",
         "com.android.internal.util.XmlUtils#convertValueToInt",
         "com.android.internal.textservice.ITextServicesManager$Stub#asInterface",

commit 8aa48c30c85ba1f922cedd88b9be7f49a3b5e679

Fix typos in previous bugfixes.
The commits 3ed37ac and 954e273 introduced minor bugs. This change fixes
them.

tools/layoutlib/bridge/src/android/widget/TimePickerSpinnerDelegate_Delegate.java
@@ -30,7 +30,7 @@ public class TimePickerSpinnerDelegate_Delegate {
     private static final int PM = 1;
 
     @LayoutlibDelegate
-    static int getAmOrPmKeyCode(int amOrPm) {
+    static int getAmOrPmKeyCode(TimePickerSpinnerDelegate tpsd, int amOrPm) {
         // We don't care about locales here.
         if (amOrPm == AM) {
             return KeyEvent.KEYCODE_A;

tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
@@ -482,7 +482,7 @@ public final class BridgeContext extends Context {
             // In some cases, style may not be a dynamic id, so we do a full search.
             ResourceReference ref = resolveId(resid);
             if (ref != null) {
-                mRenderResources.getStyle(ref.getName(), ref.isFramework());
+                style = mRenderResources.getStyle(ref.getName(), ref.isFramework());
             }
         }
 