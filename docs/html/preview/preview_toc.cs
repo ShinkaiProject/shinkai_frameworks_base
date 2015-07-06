6501dcf3a82e11b48dd1920c5fff20010d2d3f7c
Raph Levien <raph@google.com>
2015-07-06 19:17:05 +0000

    am 87798a13: am 4da2f05b: am 3be9eb46: am 247012c4: Merge "Clean up Hyphenator file reading" into mnc-dev
    * commit '87798a137ad221c4c900d0f0946559371ce00140':
      Clean up Hyphenator file reading

---
+++ docs/html/preview/preview_toc.cs
@@ -0,0 +1,88 @@
+<ul id="nav">
+
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/overview.html"
+   es-lang="Información general del programa"
+   ja-lang="プログラム概要"
+   ko-lang="프로그램 개요"
+   pt-br-lang="Visão geral do programa"
+   ru-lang="Обзор программы"
+   zh-cn-lang="计划概览"
+   zh-tw-lang="程式總覽">Program Overview
+      </a></div>
+  </li>
+
+
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/download.html">
+      Downloads</a></div>
+  </li>
+
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/setup-sdk.html">
+      Set up the SDK</a></div>
+  </li>
+
+  <li class="nav-section">
+    <div class="nav-section-header"><a href="<?cs var:toroot ?>preview/testing/guide.html">
+      Testing Guide</a></div>
+      <ul>
+        <li><a href="<?cs var:toroot ?>preview/testing/performance.html">
+          Performance Testing</a></li>
+      </ul>
+  </li>
+
+  <li class="nav-section">
+    <div class="nav-section-header"><a href="<?cs var:toroot ?>preview/api-overview.html"
+      es-lang="Información general de la API"
+      ja-lang="API の概要"
+      ko-lang="API 개요"
+      pt-br-lang="Visão geral da API"
+      ru-lang="Обзор API-интерфейсов"
+      zh-cn-lang="API 概览"
+      zh-tw-lang="API 總覽">API Overview
+      </a></div>
+      <ul>
+        <li><a href="<?cs var:toroot ?>preview/features/runtime-permissions.html"
+      es-lang="Permisos"
+      ja-lang="パーミッション"
+      ko-lang="권한"
+      pt-br-lang="Permissões"
+      ru-lang="Разрешения"
+      zh-cn-lang="权限"
+      zh-tw-lang="權限">Permissions
+          </a></li>
+        <li><a href="<?cs var:toroot ?>preview/features/app-linking.html">
+          App Links</a></li>
+        <li><a href="<?cs var:toroot ?>preview/backup/index.html">
+          Auto Backup for Apps</a></li>
+      </ul>
+  </li>
+
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/behavior-changes.html"
+      es-lang="Cambios en los comportamientos"
+      ja-lang="動作の変更点"
+      ko-lang="동작 변경"
+      pt-br-lang="Mudanças de comportamento"
+      ru-lang="Изменения в работе"
+      zh-cn-lang="行为变更"
+      zh-tw-lang="行為變更">Behavior Changes
+       </a></div>
+  </li>
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/samples.html">
+      Samples</a></div>
+  </li>
+
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/support.html">
+      Support</a></div>
+  </li>
+
+  <li class="nav-section">
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/license.html">
+      License Agreement</a></div>
+  </li>
+
+</ul>
