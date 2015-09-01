ShinkaiProject/shinkai_frameworks_base

commit 709ca4f88ba43e26c5adb061eb68c93074739a18

Band selection: Add handling for layout changes.
Previously, the app would crash druing band selection after the layout had changed.

Change-Id: I51ed2908d30e16f4d77769c798dfcef5f1ee1602

packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
@@ -573,6 +573,9 @@ public class DirectoryFragment extends Fragment {
         }
 
         mRecView.setLayoutManager(layout);
+        // TODO: Once b/23691541 is resolved, use a listener within MultiSelectManager instead of
+        // imperatively calling this function.
+        mSelectionManager.handleLayoutChanged();
         // setting layout manager automatically invalidates existing ViewHolders.
         mThumbSize = new Point(thumbSize, thumbSize);
     }

packages/DocumentsUI/src/com/android/documentsui/MultiSelectManager.java
@@ -250,6 +250,10 @@ public final class MultiSelectManager {
         notifySelectionChanged();
     }
 
+    public void handleLayoutChanged() {
+        mBandSelectManager.handleLayoutChanged();
+    }
+
     /**
      * Clears the selection, without notifying anyone.
      */
@@ -1188,6 +1192,18 @@ public final class MultiSelectManager {
             mHelper = helper;
             mHelper.addOnScrollListener(this);
             mModel = new BandSelectModel(helper);
+            mModel.addOnSelectionChangedListener(this);
+        }
+
+        /**
+         * Handle a change in layout by cleaning up and getting rid of the old model and creating
+         * a new model which will track the new layout.
+         */
+        public void handleLayoutChanged() {
+            mModel.removeOnSelectionChangedListener(this);
+            mModel.stopListening();
+
+            mModel = new BandSelectModel((RuntimeRecyclerViewHelper) mHelper);
             mModel.addOnSelectionChangedListener(this);
         }
 

commit 3f866811ba17aff78329c9e9f3f928c74447b446

Fix broken build.
Was broken by 709ca4f88ba43e26c5adb061eb68c93074739a18.

Change-Id: Ib1fcb5a8bc7aad6b3fcc501d57f19c931912af75

packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
@@ -575,7 +575,7 @@ public class DirectoryFragment extends Fragment {
         mRecView.setLayoutManager(layout);
         // TODO: Once b/23691541 is resolved, use a listener within MultiSelectManager instead of
         // imperatively calling this function.
-        mSelectionManager.handleLayoutChanged();
+        mModel.mSelectionManager.handleLayoutChanged();
         // setting layout manager automatically invalidates existing ViewHolders.
         mThumbSize = new Point(thumbSize, thumbSize);
     }