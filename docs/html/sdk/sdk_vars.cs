9adb68b1fec6a9c51de1e7eb199fdb6fa8d2a470
Cheryl Potter <cherylpotter@google.com>
2016-04-27 09:54:24 +0000

    Merge "Merge "Merge "docs: Remove obsolete deep link term from AS 2.0 Code Tools page b/28075206" into mnc-docs am: 0c43794 am: 5f9f1d0 am: a20bff4 am: ffbc170" into nyc-dev am: 10d1b1c am: 72bbce4" into nyc-mr1-dev-plus-aosp am: 71f1dd7
    * commit '71f1dd72a9a3766f3c51f0b34bf245e41667130e':
    
    Change-Id: Idf9ac8189f018b13e655c87cb4c8b105f5faebf7

---
+++ docs/html/sdk/sdk_vars.cs
@@ -0,0 +1,62 @@
+<?cs
+set:studio.version='2.0.0.20' ?><?cs
+set:studio.release.date='April 7, 2016' ?><?cs
+
+
+set:studio.linux_bundle_download='android-studio-ide-143.2739321-linux.zip' ?><?cs
+set:studio.linux_bundle_bytes='292106971' ?><?cs
+set:studio.linux_bundle_checksum='b64070ee4ec4868e9dd942b56f76864634cb0c67' ?><?cs
+
+set:studio.mac_bundle_download='android-studio-ide-143.2739321-mac.dmg' ?><?cs
+set:studio.mac_bundle_bytes='292574501' ?><?cs
+set:studio.mac_bundle_checksum='0f3d53a08815c00912c13738abc79e82207b20ed' ?><?cs
+
+set:studio.win_bundle_download='android-studio-ide-143.2739321-windows.zip' ?><?cs
+set:studio.win_bundle_bytes='294612422' ?><?cs
+set:studio.win_bundle_checksum='705c00f52b715d6a845c97979ced6f9b1b3f11c6' ?><?cs
+
+set:studio.win_bundle_exe_download='android-studio-bundle-143.2739321-windows.exe' ?><?cs
+set:studio.win_bundle_exe_bytes='1223633080' ?><?cs
+set:studio.win_bundle_exe_checksum='c556debf40de6b5d6f6d65d169a64398e3380183' ?><?cs
+
+set:studio.win_notools_exe_download='android-studio-ide-143.2739321-windows.exe' ?><?cs
+set:studio.win_notools_exe_bytes='277789224' ?><?cs
+set:studio.win_notools_exe_checksum='3e8c25bd7b7f3aa326f7b2a349c4d67c550d13ac' ?><?cs
+
+
+set:sdk.linux_download='android-sdk_r24.4.1-linux.tgz' ?><?cs
+set:sdk.linux_bytes='326412652' ?><?cs
+set:sdk.linux_checksum='725bb360f0f7d04eaccff5a2d57abdd49061326d' ?><?cs
+
+set:sdk.mac_download='android-sdk_r24.4.1-macosx.zip' ?><?cs
+set:sdk.mac_bytes='102781947' ?><?cs
+set:sdk.mac_checksum='85a9cccb0b1f9e6f1f616335c5f07107553840cd' ?><?cs
+
+set:sdk.win_download='android-sdk_r24.4.1-windows.zip' ?><?cs
+set:sdk.win_bytes='199701062' ?><?cs
+set:sdk.win_checksum='66b6a6433053c152b22bf8cab19c0f3fef4eba49' ?><?cs
+set:sdk.win_installer='installer_r24.4.1-windows.exe' ?><?cs
+set:sdk.win_installer_bytes='151659917' ?><?cs
+set:sdk.win_installer_checksum='f9b59d72413649d31e633207e31f456443e7ea0b' ?><?cs
+
+
+set:ndk.mac64_download='android-ndk-r11c-darwin-x86_64.zip' ?><?cs
+set:ndk.mac64_bytes='772428792' ?><?cs
+set:ndk.mac64_checksum='4ce8e7ed8dfe08c5fe58aedf7f46be2a97564696' ?><?cs
+
+set:ndk.linux64_download='android-ndk-r11c-linux-x86_64.zip' ?><?cs
+set:ndk.linux64_bytes='794135138' ?><?cs
+set:ndk.linux64_checksum='de5ce9bddeee16fb6af2b9117e9566352aa7e279' ?><?cs
+
+set:ndk.win64_download='android-ndk-r11c-windows-x86_64.zip' ?><?cs
+set:ndk.win64_bytes='771407642' ?><?cs
+set:ndk.win64_checksum='3d89deb97b3191c7e5555f1313ad35059479f071' ?><?cs
+set:ndk.win32_download='android-ndk-r11c-windows-x86.zip' ?><?cs
+set:ndk.win32_bytes='728899082' ?><?cs
+set:ndk.win32_checksum='ff939bde6cd374eecbd2c3b2ad218697f9a5038c'
+?>
+<?cs
+def:size_in_mb(bytes)
+  ?><?cs set:mb = bytes / 1024 / 1024
+  ?><?cs var:mb ?><?cs
+/def ?>
