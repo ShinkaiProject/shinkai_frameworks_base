mintkat/frameworks_base

[HS2.0] Need to compare with not only FQDN but SSID
When there are APs with different SSID but the same
Passpoint configuration, connection status is shown for
the multiple APs though actually a device can be connected
to only one AP.

Change-Id: Iec5e5d1deb45aad68827500c23213ec7a8342229

packages/SettingsLib/src/com/android/settingslib/wifi/AccessPoint.java
@@ -235,7 +235,7 @@ public class AccessPoint implements Comparable<AccessPoint> {
 
     public boolean matches(WifiConfiguration config) {
         if (config.isPasspoint() && mConfig != null && mConfig.isPasspoint())
-            return config.FQDN.equals(mConfig.providerFriendlyName);
+            return ssid.equals(removeDoubleQuotes(config.SSID)) && config.FQDN.equals(mConfig.providerFriendlyName);
         else
             return ssid.equals(removeDoubleQuotes(config.SSID)) && security == getSecurity(config);
     }

ShinkaiProject/shinkai_frameworks_base

Bug:32136285

packages/SettingsLib/src/com/android/settingslib/wifi/AccessPoint.java
@@ -254,7 +254,7 @@ public class AccessPoint implements Comparable<AccessPoint> {
 
     public boolean matches(WifiConfiguration config) {
         if (config.isPasspoint() && mConfig != null && mConfig.isPasspoint()) {
-            return config.FQDN.equals(mConfig.FQDN);
+            return ssid.equals(removeDoubleQuotes(config.SSID)) && config.FQDN.equals(mConfig.FQDN);
         } else {
             return ssid.equals(removeDoubleQuotes(config.SSID))
                     && security == getSecurity(config)