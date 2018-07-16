OrionOS-Project/frameworks_base

old paths contains all entries when updating
In certains circumstances, only the base and split APKs were included in
the "old paths" list when updating the application info. Instead, this
list should contain _all_ elements, including any additional libraries
that may be added to the overall classpath.

Bug: 77342775
Test: Manual. Install a package. Install a split with --dont_kill. See that the path doesn't contain duplicate entries
Change-Id: Id9739cce215ab07bff1b17966583c0cf51a0b34a

core/java/android/app/ActivityThread.java
@@ -5333,8 +5333,8 @@ public final class ActivityThread extends ClientTransactionHandler {
                                         }
                                     }
                                 }
-                                final List<String> oldPaths =
-                                        sPackageManager.getPreviousCodePaths(packageName);
+                                final ArrayList<String> oldPaths = new ArrayList<>();
+                                LoadedApk.makePaths(this, pkgInfo.getApplicationInfo(), oldPaths);
                                 pkgInfo.updateApplicationInfo(aInfo, oldPaths);
                             } catch (RemoteException e) {
                             }

ShinkaiProject/shinkai_frameworks_base

Bug: 80337129
Test: Uninstall GMS updates and reinstall. Verify that GMS Dex files aren't extrated out of the APK.
(cherry picked from commit b7022ba8d3fce5d46841972ca78751ce713d9d80)
Merged-In: Id9739cce215ab07bff1b17966583c0cf51a0b34a
Change-Id: I3c61b1b61dc1ab8a1a51a5be68f19ae9af586692

core/java/android/app/ActivityThread.java
@@ -5295,8 +5295,8 @@ public final class ActivityThread {
                                         }
                                     }
                                 }
-                                final List<String> oldPaths =
-                                        sPackageManager.getPreviousCodePaths(packageName);
+                                final ArrayList<String> oldPaths = new ArrayList<>();
+                                LoadedApk.makePaths(this, loadedApk.getApplicationInfo(), oldPaths);
                                 loadedApk.updateApplicationInfo(aInfo, oldPaths);
                             } catch (RemoteException e) {
                             }