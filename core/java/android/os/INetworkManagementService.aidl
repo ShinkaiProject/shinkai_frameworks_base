Neoteric-OS/android_frameworks_base

Support enable/disable dns forwarding of dnsmasq when start tethering
Bug: 128782815
Test: built, flashed, booted
Change-Id: Ic6ee985b42cbe77e8f2208fba62108604fc7bbcf

core/java/android/os/INetworkManagementService.aidl
@@ -150,6 +150,14 @@ interface INetworkManagementService
     void startTethering(in String[] dhcpRanges);
 
     /**
+     * Start tethering services with the specified dhcp server range and
+     * DNS proxy config.
+     * {@code String[]} is a set of start end pairs defining the ranges.
+     * {@code boolean} is used to control legacy DNS proxy server.
+     */
+    void startTetheringLegacy(boolean usingLegacyDnsProxy, in String[] dhcpRanges);
+
+    /**
      * Stop currently running tethering services
      */
     @UnsupportedAppUsage

services/core/java/com/android/server/NetworkManagementService.java
@@ -1003,11 +1003,16 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
 
     @Override
     public void startTethering(String[] dhcpRange) {
+        startTetheringLegacy(true, dhcpRange);
+    }
+
+    @Override
+    public void startTetheringLegacy(boolean usingLegacyDnsProxy, String[] dhcpRange) {
         mContext.enforceCallingOrSelfPermission(CONNECTIVITY_INTERNAL, TAG);
         // an odd number of addrs will fail
 
         try {
-            mNetdService.tetherStart(dhcpRange);
+            mNetdService.tetherStart(usingLegacyDnsProxy, dhcpRange);
         } catch (RemoteException | ServiceSpecificException e) {
             throw new IllegalStateException(e);
         }

ShinkaiProject/shinkai_frameworks_base

core/java/android/os/INetworkManagementService.aidl
@@ -150,6 +150,14 @@ interface INetworkManagementService
     void startTethering(in String[] dhcpRanges);
 
     /**
+     * Start tethering services with the specified dhcp server range and
+     * DNS proxy config.
+     * {@code boolean} is used to control legacy DNS proxy server.
+     * {@code String[]} is a set of start end pairs defining the ranges.
+     */
+    void startTetheringWithConfiguration(boolean usingLegacyDnsProxy, in String[] dhcpRanges);
+
+    /**
      * Stop currently running tethering services
      */
     @UnsupportedAppUsage

services/core/java/com/android/server/NetworkManagementService.java
@@ -1003,11 +1003,15 @@ public class NetworkManagementService extends INetworkManagementService.Stub {
 
     @Override
     public void startTethering(String[] dhcpRange) {
+        startTetheringWithConfiguration(true, dhcpRange);
+    }
+
+    @Override
+    public void startTetheringWithConfiguration(boolean usingLegacyDnsProxy, String[] dhcpRange) {
         mContext.enforceCallingOrSelfPermission(CONNECTIVITY_INTERNAL, TAG);
         // an odd number of addrs will fail
-
         try {
-            mNetdService.tetherStart(dhcpRange);
+            mNetdService.tetherStartWithConfiguration(usingLegacyDnsProxy, dhcpRange);
         } catch (RemoteException | ServiceSpecificException e) {
             throw new IllegalStateException(e);
         }