Neoteric-OS/android_frameworks_base

Add multi client ror support
Detailed design in http://go/multi-client-ror. This cl adds internal
states in RecoverySystemService to keep track of multiple RoR
callers. The general principles works as:
1. Each client should call prepare before reboot
2. One client cannot clear the preparation of another client
3. If multiple clients have prepared the update, the subsequent
   reboot will be first come, first served.

Bug: 170664917
Test: atest FrameworksServicesTests:RecoverySystemServiceTest

Change-Id: Ic8c7438f8a0300870ceed8857c46425fc0d006b9

core/java/android/os/IRecoverySystem.aidl
@@ -27,7 +27,8 @@ interface IRecoverySystem {
     boolean setupBcb(in String command);
     boolean clearBcb();
     void rebootRecoveryWithCommand(in String command);
-    boolean requestLskf(in String updateToken, in IntentSender sender);
-    boolean clearLskf();
-    boolean rebootWithLskf(in String updateToken, in String reason);
+    boolean requestLskf(in String callerId, in IntentSender sender);
+    boolean clearLskf(in String callerId);
+    boolean isLskfCaptured(in String callerId);
+    boolean rebootWithLskf(in String callerId, in String reason, in boolean slotSwitch);
 }

ShinkaiProject/shinkai_frameworks_base

core/java/android/os/IRecoverySystem.aidl
@@ -27,7 +27,8 @@ interface IRecoverySystem {
     boolean setupBcb(in String command);
     boolean clearBcb();
     void rebootRecoveryWithCommand(in String command);
-    boolean requestLskf(in String updateToken, in IntentSender sender);
-    boolean clearLskf();
-    boolean rebootWithLskf(in String updateToken, in String reason);
+    boolean requestLskf(in String packageName, in IntentSender sender);
+    boolean clearLskf(in String packageName);
+    boolean isLskfCaptured(in String packageName);
+    boolean rebootWithLskf(in String packageName, in String reason, in boolean slotSwitch);
 }