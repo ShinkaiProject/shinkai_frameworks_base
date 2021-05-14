872cc3128965d205c5ffd2896f0e361b124661a3
TreeHugger Robot <treehugger-gerrit@google.com>
2021-05-14 10:25:22 +0000

    Merge "Address comments on ag/14486203" into sc-dev am: 76922458e7
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/base/+/14540395
    
    Change-Id: I4d1abc37dbb5a43cb1db58f3409c443607b68e65

---
+++ core/java/android/hardware/lights/Light.java
@@ -0,0 +1,183 @@
+/**
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.hardware.lights;
+
+import android.annotation.IntDef;
+import android.annotation.NonNull;
+import android.annotation.Nullable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Represents a logical light on the device.
+ *
+ */
+public final class Light implements Parcelable {
+    // These enum values copy the values from {@link com.android.server.lights.LightsManager}
+    // and the light HAL. Since 0-7 are lights reserved for system use, 8 for microphone light is
+    // defined in {@link android.hardware.lights.LightsManager}, following types are available
+    // through this API.
+    /** Type for lights that indicate microphone usage */
+    public static final int LIGHT_TYPE_MICROPHONE = 8;
+
+    // These enum values start from 10001 to avoid collision with expanding of HAL light types.
+    /**
+     * Type for lights that indicate a monochrome color LED light.
+     */
+    public static final int LIGHT_TYPE_INPUT_SINGLE = 10001;
+
+    /**
+     * Type for lights that indicate a group of LED lights representing player ID.
+     * Player ID lights normally present on game controllers are lights that consist of a row of
+     * LEDs.
+     * During multi-player game, the player ID for the current game controller is represented by
+     * one of the LED that is lit according to its position in the row.
+     */
+    public static final int LIGHT_TYPE_INPUT_PLAYER_ID = 10002;
+
+    /**
+     * Type for lights that indicate a color LED light.
+     */
+    public static final int LIGHT_TYPE_INPUT_RGB = 10003;
+
+    /** @hide */
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef(prefix = {"LIGHT_TYPE_"},
+        value = {
+            LIGHT_TYPE_INPUT_PLAYER_ID,
+            LIGHT_TYPE_INPUT_SINGLE,
+            LIGHT_TYPE_INPUT_RGB,
+        })
+    public @interface LightType {}
+
+    private final int mId;
+    private final int mOrdinal;
+    private final int mType;
+    private final String mName;
+
+    /**
+     * Creates a new light with the given data.
+     *
+     * @hide
+     */
+    public Light(int id, int ordinal, int type) {
+        this(id, ordinal, type, "Light");
+    }
+
+    /**
+     * Creates a new light with the given data.
+     *
+     * @hide
+     */
+    public Light(int id, int ordinal, int type, String name) {
+        mId = id;
+        mOrdinal = ordinal;
+        mType = type;
+        mName = name;
+    }
+
+    private Light(@NonNull Parcel in) {
+        mId = in.readInt();
+        mOrdinal = in.readInt();
+        mType = in.readInt();
+        mName = in.readString();
+    }
+
+    /** Implement the Parcelable interface */
+    @Override
+    public void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeInt(mId);
+        dest.writeInt(mOrdinal);
+        dest.writeInt(mType);
+        dest.writeString(mName);
+    }
+
+    /** Implement the Parcelable interface */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /** Implement the Parcelable interface */
+    public static final @android.annotation.NonNull Parcelable.Creator<Light> CREATOR =
+            new Parcelable.Creator<Light>() {
+                public Light createFromParcel(Parcel in) {
+                    return new Light(in);
+                }
+
+                public Light[] newArray(int size) {
+                    return new Light[size];
+                }
+            };
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (obj instanceof Light) {
+            Light light = (Light) obj;
+            return mId == light.mId && mOrdinal == light.mOrdinal && mType == light.mType;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return mId;
+    }
+
+    @Override
+    public String toString() {
+        return "[Name=" + mName + " Id=" + mId + " Type=" + mType + " Ordinal=" + mOrdinal + "]";
+    }
+
+    /**
+     * Returns the id of the light.
+     *
+     * <p>This is an opaque value used as a unique identifier for the light.
+     */
+    public int getId() {
+        return mId;
+    }
+
+    /**
+     * Returns the name of the light.
+     */
+    @NonNull
+    public String getName() {
+        return mName;
+    }
+
+    /**
+     * Returns the ordinal of the light.
+     *
+     * <p>This is a sort key that represents the physical order of lights on the device with the
+     * same type. In the case of multiple lights arranged in a line, for example, the ordinals
+     * could be [1, 2, 3, 4], or [0, 10, 20, 30], or any other values that have the same sort order.
+     */
+    public int getOrdinal() {
+        return mOrdinal;
+    }
+
+    /**
+     * Returns the logical type of the light.
+     */
+    public @LightsManager.LightType int getType() {
+        return mType;
+    }
+}
---
+++ core/java/android/os/PowerComponents.java
@@ -0,0 +1,387 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package android.os;
+
+import android.annotation.NonNull;
+
+import com.android.internal.os.PowerCalculator;
+
+import java.io.PrintWriter;
+
+/**
+ * Contains details of battery attribution data broken down to individual power drain types
+ * such as CPU, RAM, GPU etc.
+ *
+ * @hide
+ */
+class PowerComponents {
+    private static final int CUSTOM_POWER_COMPONENT_OFFSET = BatteryConsumer.POWER_COMPONENT_COUNT
+            - BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID;
+
+    private final double mConsumedPowerMah;
+    private final double[] mPowerComponentsMah;
+    private final long[] mUsageDurationsMs;
+    private final int mCustomPowerComponentCount;
+    private final byte[] mPowerModels;
+    // Not written to Parcel and must be explicitly restored during the parent object's unparceling
+    private String[] mCustomPowerComponentNames;
+
+    PowerComponents(@NonNull Builder builder) {
+        mCustomPowerComponentNames = builder.mCustomPowerComponentNames;
+        mCustomPowerComponentCount = mCustomPowerComponentNames.length;
+        mPowerComponentsMah = builder.mPowerComponentsMah;
+        mUsageDurationsMs = builder.mUsageDurationsMs;
+        mConsumedPowerMah = builder.getTotalPower();
+        mPowerModels = builder.mPowerModels;
+    }
+
+    PowerComponents(@NonNull Parcel source) {
+        mConsumedPowerMah = source.readDouble();
+        mCustomPowerComponentCount = source.readInt();
+        mPowerComponentsMah = source.createDoubleArray();
+        mUsageDurationsMs = source.createLongArray();
+        if (source.readBoolean()) {
+            mPowerModels = new byte[BatteryConsumer.POWER_COMPONENT_COUNT];
+            source.readByteArray(mPowerModels);
+        } else {
+            mPowerModels = null;
+        }
+    }
+
+    /** Writes contents to Parcel */
+    void writeToParcel(@NonNull Parcel dest, int flags) {
+        dest.writeDouble(mConsumedPowerMah);
+        dest.writeInt(mCustomPowerComponentCount);
+        dest.writeDoubleArray(mPowerComponentsMah);
+        dest.writeLongArray(mUsageDurationsMs);
+        if (mPowerModels != null) {
+            dest.writeBoolean(true);
+            dest.writeByteArray(mPowerModels);
+        } else {
+            dest.writeBoolean(false);
+        }
+    }
+
+    /**
+     * Total power consumed by this consumer, in mAh.
+     */
+    public double getConsumedPower() {
+        return mConsumedPowerMah;
+    }
+
+    /**
+     * Returns the amount of drain attributed to the specified drain type, e.g. CPU, WiFi etc.
+     *
+     * @param componentId The ID of the power component, e.g.
+     *                    {@link BatteryConsumer#POWER_COMPONENT_CPU}.
+     * @return Amount of consumed power in mAh.
+     */
+    public double getConsumedPower(@BatteryConsumer.PowerComponent int componentId) {
+        if (componentId >= BatteryConsumer.POWER_COMPONENT_COUNT) {
+            throw new IllegalArgumentException(
+                    "Unsupported power component ID: " + componentId);
+        }
+        try {
+            return mPowerComponentsMah[componentId];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Unsupported power component ID: " + componentId);
+        }
+    }
+
+    /**
+     * Returns the amount of drain attributed to the specified custom drain type.
+     *
+     * @param componentId The ID of the custom power component.
+     * @return Amount of consumed power in mAh.
+     */
+    public double getConsumedPowerForCustomComponent(int componentId) {
+        if (componentId >= BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID
+                && componentId < BatteryConsumer.LAST_CUSTOM_POWER_COMPONENT_ID) {
+            try {
+                return mPowerComponentsMah[CUSTOM_POWER_COMPONENT_OFFSET + componentId];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException(
+                        "Unsupported custom power component ID: " + componentId);
+            }
+        } else {
+            throw new IllegalArgumentException(
+                    "Unsupported custom power component ID: " + componentId);
+        }
+    }
+
+    void setCustomPowerComponentNames(String[] customPowerComponentNames) {
+        mCustomPowerComponentNames = customPowerComponentNames;
+    }
+
+    public String getCustomPowerComponentName(int componentId) {
+        if (componentId >= BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID
+                && componentId < BatteryConsumer.LAST_CUSTOM_POWER_COMPONENT_ID) {
+            try {
+                return mCustomPowerComponentNames[componentId
+                        - BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID];
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException(
+                        "Unsupported custom power component ID: " + componentId);
+            }
+        } else {
+            throw new IllegalArgumentException(
+                    "Unsupported custom power component ID: " + componentId);
+        }
+    }
+
+    @BatteryConsumer.PowerModel
+    int getPowerModel(@BatteryConsumer.PowerComponent int component) {
+        if (mPowerModels == null) {
+            throw new IllegalStateException(
+                    "Power model IDs were not requested in the BatteryUsageStatsQuery");
+        }
+        return mPowerModels[component];
+    }
+
+    /**
+     * Returns the amount of time used by the specified component, e.g. CPU, WiFi etc.
+     *
+     * @param componentId The ID of the power component, e.g.
+     *                    {@link BatteryConsumer#POWER_COMPONENT_CPU}.
+     * @return Amount of time in milliseconds.
+     */
+    public long getUsageDurationMillis(@BatteryConsumer.PowerComponent int componentId) {
+        if (componentId >= BatteryConsumer.POWER_COMPONENT_COUNT) {
+            throw new IllegalArgumentException(
+                    "Unsupported power component ID: " + componentId);
+        }
+        try {
+            return mUsageDurationsMs[componentId];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("Unsupported power component ID: " + componentId);
+        }
+    }
+
+    /**
+     * Returns the amount of usage time attributed to the specified custom component.
+     *
+     * @param componentId The ID of the custom power component.
+     * @return Amount of time in milliseconds.
+     */
+    public long getUsageDurationForCustomComponentMillis(int componentId) {
+        if (componentId < BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID) {
+            throw new IllegalArgumentException(
+                    "Unsupported custom power component ID: " + componentId);
+        }
+        try {
+            return mUsageDurationsMs[CUSTOM_POWER_COMPONENT_OFFSET + componentId];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException(
+                    "Unsupported custom power component ID: " + componentId);
+        }
+    }
+
+    public int getCustomPowerComponentCount() {
+        return mCustomPowerComponentCount;
+    }
+
+    /**
+     * Returns the largest usage duration among all power components.
+     */
+    public long getMaxComponentUsageDurationMillis() {
+        long max = 0;
+        for (int i = mUsageDurationsMs.length - 1; i >= 0; i--) {
+            if (mUsageDurationsMs[i] > max) {
+                max = mUsageDurationsMs[i];
+            }
+        }
+        return max;
+    }
+
+    public void dump(PrintWriter pw, boolean skipEmptyComponents) {
+        String separator = "";
+        for (int componentId = 0; componentId < BatteryConsumer.POWER_COMPONENT_COUNT;
+                componentId++) {
+            final double componentPower = getConsumedPower(componentId);
+            if (skipEmptyComponents && componentPower == 0) {
+                continue;
+            }
+            pw.print(separator); separator = " ";
+            pw.print(BatteryConsumer.powerComponentIdToString(componentId));
+            pw.print("=");
+            PowerCalculator.printPowerMah(pw, componentPower);
+        }
+
+        final int customComponentCount = getCustomPowerComponentCount();
+        for (int customComponentId = BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID;
+                customComponentId < BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID
+                        + customComponentCount;
+                customComponentId++) {
+            final double customComponentPower =
+                    getConsumedPowerForCustomComponent(customComponentId);
+            if (skipEmptyComponents && customComponentPower == 0) {
+                continue;
+            }
+            pw.print(separator); separator = " ";
+            pw.print(getCustomPowerComponentName(customComponentId));
+            pw.print("=");
+            PowerCalculator.printPowerMah(pw, customComponentPower);
+        }
+    }
+
+    /**
+     * Builder for PowerComponents.
+     */
+    static final class Builder {
+        private final double[] mPowerComponentsMah;
+        private final String[] mCustomPowerComponentNames;
+        private final long[] mUsageDurationsMs;
+        private final byte[] mPowerModels;
+
+        Builder(@NonNull String[] customPowerComponentNames, boolean includePowerModels) {
+            mCustomPowerComponentNames = customPowerComponentNames;
+            int powerComponentCount =
+                    BatteryConsumer.POWER_COMPONENT_COUNT + mCustomPowerComponentNames.length;
+            mPowerComponentsMah = new double[powerComponentCount];
+            mUsageDurationsMs = new long[powerComponentCount];
+            if (includePowerModels) {
+                mPowerModels = new byte[BatteryConsumer.POWER_COMPONENT_COUNT];
+            } else {
+                mPowerModels = null;
+            }
+        }
+
+        /**
+         * Sets the amount of drain attributed to the specified drain type, e.g. CPU, WiFi etc.
+         *
+         * @param componentId    The ID of the power component, e.g.
+         *                       {@link BatteryConsumer#POWER_COMPONENT_CPU}.
+         * @param componentPower Amount of consumed power in mAh.
+         */
+        @NonNull
+        public Builder setConsumedPower(@BatteryConsumer.PowerComponent int componentId,
+                double componentPower, @BatteryConsumer.PowerModel int powerModel) {
+            if (componentId >= BatteryConsumer.POWER_COMPONENT_COUNT) {
+                throw new IllegalArgumentException(
+                        "Unsupported power component ID: " + componentId);
+            }
+            try {
+                mPowerComponentsMah[componentId] = componentPower;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException(
+                        "Unsupported power component ID: " + componentId);
+            }
+            if (mPowerModels != null) {
+                mPowerModels[componentId] = (byte) powerModel;
+            }
+            return this;
+        }
+
+        /**
+         * Sets the amount of drain attributed to the specified custom drain type.
+         *
+         * @param componentId    The ID of the custom power component.
+         * @param componentPower Amount of consumed power in mAh.
+         */
+        @NonNull
+        public Builder setConsumedPowerForCustomComponent(int componentId, double componentPower) {
+            if (componentId >= BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID
+                    && componentId < BatteryConsumer.LAST_CUSTOM_POWER_COMPONENT_ID) {
+                try {
+                    mPowerComponentsMah[CUSTOM_POWER_COMPONENT_OFFSET + componentId] =
+                            componentPower;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new IllegalArgumentException(
+                            "Unsupported custom power component ID: " + componentId);
+                }
+            } else {
+                throw new IllegalArgumentException(
+                        "Unsupported custom power component ID: " + componentId);
+            }
+            return this;
+        }
+
+        /**
+         * Sets the amount of time used by the specified component, e.g. CPU, WiFi etc.
+         *
+         * @param componentId                  The ID of the power component, e.g.
+         *                                     {@link BatteryConsumer#POWER_COMPONENT_CPU}.
+         * @param componentUsageDurationMillis Amount of time in milliseconds.
+         */
+        @NonNull
+        public Builder setUsageDurationMillis(@BatteryConsumer.PowerComponent int componentId,
+                long componentUsageDurationMillis) {
+            if (componentId >= BatteryConsumer.POWER_COMPONENT_COUNT) {
+                throw new IllegalArgumentException(
+                        "Unsupported power component ID: " + componentId);
+            }
+            try {
+                mUsageDurationsMs[componentId] = componentUsageDurationMillis;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException(
+                        "Unsupported power component ID: " + componentId);
+            }
+            return this;
+        }
+
+        /**
+         * Sets the amount of time used by the specified custom component.
+         *
+         * @param componentId                  The ID of the custom power component.
+         * @param componentUsageDurationMillis Amount of time in milliseconds.
+         */
+        @NonNull
+        public Builder setUsageDurationForCustomComponentMillis(int componentId,
+                long componentUsageDurationMillis) {
+            if (componentId < BatteryConsumer.FIRST_CUSTOM_POWER_COMPONENT_ID) {
+                throw new IllegalArgumentException(
+                        "Unsupported custom power component ID: " + componentId);
+            }
+            try {
+                mUsageDurationsMs[CUSTOM_POWER_COMPONENT_OFFSET + componentId] =
+                        componentUsageDurationMillis;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new IllegalArgumentException(
+                        "Unsupported custom power component ID: " + componentId);
+            }
+            return this;
+        }
+
+        public void addPowerAndDuration(Builder other) {
+            for (int i = mPowerComponentsMah.length - 1; i >= 0; i--) {
+                mPowerComponentsMah[i] += other.mPowerComponentsMah[i];
+            }
+            for (int i = mUsageDurationsMs.length - 1; i >= 0; i--) {
+                mUsageDurationsMs[i] += other.mUsageDurationsMs[i];
+            }
+        }
+
+        /**
+         * Returns the total power accumulated by this builder so far. It may change
+         * by the time the {@code build()} method is called.
+         */
+        public double getTotalPower() {
+            double totalPowerMah = 0;
+            for (int i = mPowerComponentsMah.length - 1; i >= 0; i--) {
+                totalPowerMah += mPowerComponentsMah[i];
+            }
+            return totalPowerMah;
+        }
+
+        /**
+         * Creates a read-only object out of the Builder values.
+         */
+        @NonNull
+        public PowerComponents build() {
+            return new PowerComponents(this);
+        }
+    }
+}
