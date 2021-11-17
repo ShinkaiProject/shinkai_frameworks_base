Neoteric-OS/android_frameworks_base

[PeopleService] Catch exception when failed to query call log.
It's unnecessary to throw exception when failed to query call
log in PeopleService. So catch this exception to avoid system
server crash.

Bug: 204923224
Test: PeopleServiceTest
Change-Id: I77ee31ff456455721c3401f36d4a79390db2f9b5

services/people/java/com/android/server/people/data/CallLogQueryHelper.java
@@ -93,6 +93,9 @@ class CallLogQueryHelper {
                     hasResults = true;
                 }
             }
+        } catch (SecurityException ex) {
+            Slog.w(TAG, "Query call log failed: " + ex);
+            return false;
         }
         return hasResults;
     }

ShinkaiProject/shinkai_frameworks_base

services/people/java/com/android/server/people/data/CallLogQueryHelper.java
@@ -93,6 +93,9 @@ class CallLogQueryHelper {
                     hasResults = true;
                 }
             }
+        } catch (SecurityException ex) {
+            Slog.e(TAG, "Query call log failed: " + ex);
+            return false;
         }
         return hasResults;
     }