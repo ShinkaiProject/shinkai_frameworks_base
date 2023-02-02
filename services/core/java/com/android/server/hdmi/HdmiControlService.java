Neoteric-OS/android_frameworks_base

HDMICEC: Call setArcStatus in setArcMode
If the TV is available, call setArcStatus also when binder service's
setArcMode is called.

Bug: 177973980
Test: adb shell cmd hdmi_control setarc on
Change-Id: I60060bdd37df30b25963c012a0d5e4549bf45ff7

services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -2056,6 +2056,7 @@ public class HdmiControlService extends SystemService {
                         Slog.w(TAG, "Local tv device not available to change arc mode.");
                         return;
                     }
+                    tv.setArcStatus(enabled);
                 }
             });
         }

ShinkaiProject/shinkai_frameworks_base

Bug: 187168483

services/core/java/com/android/server/hdmi/HdmiControlService.java
@@ -2056,6 +2056,7 @@ public class HdmiControlService extends SystemService {
                         Slog.w(TAG, "Local tv device not available to change arc mode.");
                         return;
                     }
+                    tv.startArcAction(enabled);
                 }
             });
         }