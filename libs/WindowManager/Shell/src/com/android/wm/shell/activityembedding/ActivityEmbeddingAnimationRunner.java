ShinkaiProject/shinkai_frameworks_base

commit 15435f984b4cd3c4c27accf572b0e0233cdd7968

Do not add edge extension if the container is translucent
The edge extension could be added to the dimming layer while
starting a dialog activity with the window dimming behind

Bug: 319169778
Test: Starts a dialog style embedded activity
Change-Id: I1d171344a09cbdb3a6518c2609feac7edd937ca2

libs/WindowManager/Shell/src/com/android/wm/shell/activityembedding/ActivityEmbeddingAnimationRunner.java
@@ -20,6 +20,7 @@ import static android.view.WindowManager.TRANSIT_CHANGE;
 import static android.view.WindowManager.TRANSIT_CLOSE;
 import static android.view.WindowManagerPolicyConstants.TYPE_LAYER_OFFSET;
 import static android.window.TransitionInfo.FLAG_IS_BEHIND_STARTING_WINDOW;
+import static android.window.TransitionInfo.FLAG_TRANSLUCENT;
 
 import static com.android.wm.shell.activityembedding.ActivityEmbeddingAnimationSpec.createShowSnapshotForClosingAnimation;
 import static com.android.wm.shell.transition.TransitionAnimationHelper.addBackgroundToTransition;
@@ -328,6 +329,9 @@ class ActivityEmbeddingAnimationRunner {
         for (ActivityEmbeddingAnimationAdapter adapter : adapters) {
             final Animation animation = adapter.mAnimation;
             if (!animation.hasExtension()) {
+                continue;
+            }
+            if (adapter.mChange.hasFlags(FLAG_TRANSLUCENT)) {
                 continue;
             }
             final TransitionInfo.Change change = adapter.mChange;

commit a60a34d0d671fcc3c9544e4a25c18f101c6b04d3

Fixes no edge extension in Settings
This is a regression by commit 15435f9. Clicking on the list
items on the primary TF on Settings will start a new and adjacent
secondary TF. In that case, the original secondary TF no longer
has an adjacent TF and therefore is evaluated as a translucent
change.

Skip adding edge extension for translucent Activity only.

Bug: 319169778
Test: verify on Settings
Change-Id: I16311c31f2b83c246e96c2c01975f45011548fa4

libs/WindowManager/Shell/src/com/android/wm/shell/activityembedding/ActivityEmbeddingAnimationRunner.java
@@ -331,7 +331,9 @@ class ActivityEmbeddingAnimationRunner {
             if (!animation.hasExtension()) {
                 continue;
             }
-            if (adapter.mChange.hasFlags(FLAG_TRANSLUCENT)) {
+            if (adapter.mChange.hasFlags(FLAG_TRANSLUCENT)
+                    && adapter.mChange.getActivityComponent() != null) {
+                // Skip edge extension for translucent activity.
                 continue;
             }
             final TransitionInfo.Change change = adapter.mChange;