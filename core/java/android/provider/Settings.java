dc7aa16338c6b81df423423587ecd64c3de82cd2
Abhay Singh Gill <abhaygill017@gmail.com>
2025-10-19 22:05:26 +0545

    core: Make PIF user configurable [1/2]
    Separated out from [1].
    
    [1]: https://github.com/Neoteric-OS/android_frameworks_base/commit/1fefdf1ea4d74bbe2ff427a499321c5b0959a6cd
    
    Change-Id: Ie86218a5034ff5c6fa9ec3e00a925ffd750686c9
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14099,6 +14099,20 @@ public final class Settings {
         public static final String MEDIA_SQUIGGLE_ANIMATION = "media_squiggle_animation";
 
         /**
+         * User selectable PIF data.
+         * @hide
+         */
+        @Readable
+        public static final String PIF_DATA = "pif_data";
+
+        /**
+         * PIF data fetched from our repo.
+         * @hide
+         */
+        @Readable
+        public static final String FETCHED_PIF = "fetched_pif";
+
+        /**
          * User selectable keybox data.
          * @hide
          */
